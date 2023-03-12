aaf15d172748ffc5fcceeb4b3c35a5ef404f84bf
Roman Liukevich <r.liukevich@gmail.com>
2023-03-12 16:14:21 +0100

    Initial Commit

---
+++ Meow Matcher/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Meow Matcher
+//
+//  Created by Roman Liukevich on 3/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Meow Matcher/Meow_MatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meow_MatcherApp.swift
+//  Meow Matcher
+//
+//  Created by Roman Liukevich on 3/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Meow_MatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
